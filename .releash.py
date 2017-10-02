cfb01c9095c1429ab53820820ba8c9f4aa9a87cb
maartenbreddels <maartenbreddels@gmail.com>
2017-10-02 14:27:34 +0200

    tag in releasing, don't use the fake conda feedstock

--- .releash.py
+++ .releash.py
@@ -1,8 +1,8 @@
 from releash import *
 myself = add_package(".", "releash")
 myself.version_source = VersionSource(myself, 'releash.py')
-myself.version_targets.append(VersionTargetGitTag(myself))
 myself.version_targets.append(VersionTarget(myself, 'releash.py'))
 myself.release_targets.append(ReleaseTargetSourceDist(myself))
+myself.release_targets.append(ReleaseTargetGitTagVersion(myself))
 myself.release_targets.append(ReleaseTargetGitPush(myself))
-myself.release_targets.append(ReleaseTargetCondaForge(myself, 'releash-fake-feedstock'))
+#myself.release_targets.append(ReleaseTargetCondaForge(myself, 'releash-fake-feedstock'))
