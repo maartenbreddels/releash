ee2b35abb1547ac375ec7618a24787452a7ce9a0
maartenbreddels <maartenbreddels@gmail.com>
2017-10-25 13:36:06 +0200

    Release 0.1.7

--- releash.py
+++ releash.py
@@ -20,8 +20,8 @@ except ImportError:
     from urllib.request import urlretrieve  # py3
     from urllib.error import HTTPError
 
-__version_tuple__ = (0, 1, 6)
-__version__ = '0.1.6'
+__version_tuple__ = (0, 1, 7)
+__version__ = '0.1.7'
 
 try:
     input = raw_input  # py2/3
